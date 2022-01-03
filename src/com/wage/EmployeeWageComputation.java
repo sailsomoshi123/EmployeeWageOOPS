218795b8d45c84735e51e2ed6ec563bcb77529ab
Sail Somoshi <sailsomoshi11@gmail.com>
2022-01-03 22:55:25 +0530

    ADDED: Day and hour condition to employee wage

--- src/com/wage/EmployeeWageComputation.java
+++ src/com/wage/EmployeeWageComputation.java
@@ -4,21 +4,23 @@ import java.util.Scanner;
 public class EmployeeWageComputation {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-
+    public static final int MAX_HOURS_IN_MONTH=100;
+    public static final int NUM_OF_DAYS = 20;
     public static void main(String[] args) {
         EmployeeWageComputation totalWage = new EmployeeWageComputation();
         totalWage.totalEmpWage();
     }
     void totalEmpWage() {
         System.out.println("welcome to employee wage computation");
-        final int NUM_OF_DAYS = 20;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a employee rate per hour:");
         int empRatePerHour= sc.nextInt();
-        int empWages = 0;
         int empHour = 0;
         int totalEmpWages = 0;
-        for ( int day=0; day<=NUM_OF_DAYS; day++) {
+        int totalWorkingDays = 0;
+        int totalEmpHours = 0;
+        while ( totalEmpHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_DAYS ) {
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() *10)%3;
             switch (empCheck) {
                 case IS_FULL_TIME:
@@ -30,10 +32,10 @@ public class EmployeeWageComputation {
                 default:
                     empHour = 0;
             }
-            empWages = empHour * empRatePerHour;
-            totalEmpWages += empWages;
-            System.out.println(" employee wage is " + empWages);
+            System.out.println("day : " + totalWorkingDays + " emp hour : " + empHour );
+            totalEmpHours+= empHour;
         }
+        totalEmpWages = totalEmpHours * empRatePerHour;
         System.out.println("total employee wage is " + totalEmpWages);
     }
 }
