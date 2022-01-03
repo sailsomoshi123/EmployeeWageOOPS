37b78d90450d73b3d2674edb44f57e95c3cb02c1
Sail Somoshi <sailsomoshi11@gmail.com>
2022-01-03 22:39:10 +0530

    ADDED: Program for monthly wage computation

--- src/com/wage/EmployeeWageComputation.java
+++ src/com/wage/EmployeeWageComputation.java
@@ -4,32 +4,36 @@ import java.util.Scanner;
 public class EmployeeWageComputation {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-    int empHour = 0;
+
     public static void main(String[] args) {
-        EmployeeWageComputation empHour= new EmployeeWageComputation();
-        EmployeeWageComputation empWage = new EmployeeWageComputation();
-        empWage.dailyEmployeeWage(empHour.employeeHour());
+        EmployeeWageComputation totalWage = new EmployeeWageComputation();
+        totalWage.totalEmpWage();
     }
-    void dailyEmployeeWage (int empHour) {
+    void totalEmpWage() {
         System.out.println("welcome to employee wage computation");
+        final int NUM_OF_DAYS = 20;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter employee rate per hour: ");
-        int empRatePerHour = sc.nextInt();
-        int empWages = empHour * empRatePerHour;
-        System.out.println("employee wage is " + empWages);
-    }
-    int employeeHour() {
-        int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHour = 8;
-                break;
-            case IS_PART_TIME:
-                empHour = 4;
-                break;
-            default:
-                empHour = 0;
+        System.out.println("Enter a employee rate per hour:");
+        int empRatePerHour= sc.nextInt();
+        int empWages = 0;
+        int empHour = 0;
+        int totalEmpWages = 0;
+        for ( int day=0; day<=NUM_OF_DAYS; day++) {
+            int empCheck = (int) Math.floor(Math.random() *10)%3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHour = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHour = 4;
+                    break;
+                default:
+                    empHour = 0;
+            }
+            empWages = empHour * empRatePerHour;
+            totalEmpWages += empWages;
+            System.out.println(" employee wage is " + empWages);
         }
-        return empHour;
+        System.out.println("total employee wage is " + totalEmpWages);
     }
 }
