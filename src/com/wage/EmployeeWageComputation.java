a109a8d8e1eebc95a9cd487cd2c59f2ed5b3cadd
Sail Somoshi <sailsomoshi11@gmail.com>
2022-01-03 19:29:59 +0530

    ADDED:Welcome massage to master

---
+++ src/com/wage/EmployeeWageComputation.java
@@ -0,0 +1,7 @@
+package com.wage;
+
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage computation");
+    }
+}
