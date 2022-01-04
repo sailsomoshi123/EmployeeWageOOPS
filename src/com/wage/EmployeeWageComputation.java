deeb89a692b36861a0191ec844c800cbdcb327e6
Sail Somoshi <sailsomoshi11@gmail.com>
2022-01-04 06:49:09 +0530

    ADDED: Method to calculate wage

--- src/com/wage/EmployeeWageComputation.java
+++ src/com/wage/EmployeeWageComputation.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 public class EmployeeWageComputation {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
-    public static final int MAX_HOURS_IN_MONTH=100;
-    public static final int NUM_OF_DAYS = 20;
     public static void main(String[] args) {
         EmployeeWageComputation totalWage = new EmployeeWageComputation();
         totalWage.totalEmpWage();
@@ -15,11 +13,15 @@ public class EmployeeWageComputation {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a employee rate per hour:");
         int empRatePerHour= sc.nextInt();
+        System.out.println("Enter a number of days to calculate wage : ");
+        int numOfDays = sc.nextInt();
+        System.out.println("Enter a max Working hour : ");
+        int maxWorkingHour = sc.nextInt();
         int empHour = 0;
         int totalEmpWages = 0;
         int totalWorkingDays = 0;
         int totalEmpHours = 0;
-        while ( totalEmpHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_DAYS ) {
+        while ( totalEmpHours <= maxWorkingHour && totalWorkingDays < numOfDays ) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() *10)%3;
             switch (empCheck) {
